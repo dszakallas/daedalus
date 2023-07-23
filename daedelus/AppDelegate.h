8614b6b9ede47bcf91e235366026165e6fa9bba0
Dávid Szakállas <david.szakallas@gmail.com>
2023-07-23 23:35:33 +0200

    Initial Commit

---
+++ daedelus/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  daedelus
+//
+//  Created by Dávid Szakállas on 2023. 07. 23..
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ daedelus/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  daedelus
+//
+//  Created by Dávid Szakállas on 2023. 07. 23..
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
